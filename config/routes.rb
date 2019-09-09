b9e1c688c1e59165100a37ec8ea99fb771a6d269
Moses Ogwo <mosesogwo@gmail.com>
2019-09-09 20:26:53 +0100

    add signup feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'user/new'
+  # get 'signup', to: 'users#new'
+  # post 'signup', to: 'users#create'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   resources :posts, only: [:new, :create, :index]
+  resources :users, only: [:new, :create]
 end
