322299097b640b08b7079ac5c42b49af971cf7a0
Moses Ogwo <mosesogwo@gmail.com>
2019-09-09 16:08:42 +0100

    add Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
