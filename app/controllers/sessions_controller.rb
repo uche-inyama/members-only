31710cb8b2b1d42277ae368cb72b6bcd3044e874
uche-inyama <depoulet@yahoo.com>
2019-09-09 13:30:54 +0100

    add create action on sessions controller & logic for comparing user entered password with password_digest

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,13 @@ class SessionsController < ApplicationController
   def new
    
   end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password]) 
+      log_in @user
+    else                    
+      render 'new'
+    end
+  end
 end
