b95c4314cd92f02a5604dc0ca56fbbb55780154e
Moses Ogwo <mosesogwo@gmail.com>
2019-09-09 12:37:58 +0100

    add before_create :create_remember_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ApplicationRecord
+  before_create :create_remember_digest
 
-has_secure_password
+  has_secure_password
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_digest
+      token = User.new_token
+      self.remember_digest = User.digest(token.to_s)
+    end
 end
